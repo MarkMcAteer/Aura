13c450bcfdcfb1b003ce323e6a3bfcdd6fd34ff1
MarkMcAteer <markjmcateer@gmail.com>
2024-12-19 23:22:04 -0800

    12. Gamemode
    Aura can now runaround with the camera correectly watching her.

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright McAteer Studio
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright McAteer Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
